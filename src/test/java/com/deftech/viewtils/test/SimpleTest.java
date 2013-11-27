e76f56c55d051967cf6c6cd5678563f996ca6829
kbremner <kbremner@live.co.uk>
2013-11-27 12:53:38 +0000

    Changed to statically importing Helper.with in SimpleTest

--- src/test/java/com/deftech/viewtils/test/SimpleTest.java
+++ src/test/java/com/deftech/viewtils/test/SimpleTest.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.deftech.viewtils.helpers.Helper;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
@@ -13,6 +12,7 @@ import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertNotNull;
+import static com.deftech.viewtils.helpers.Helper.with;
 
 
 
@@ -20,25 +20,22 @@ import static org.junit.Assert.assertNotNull;
 @Config(manifest = "src/test/resources/AndroidManifest.xml")
 public class SimpleTest {
 
-  @org.junit.Test
-  public void testSomething() throws Exception {
-    Activity activity = Robolectric.buildActivity(SimpleActivity.class).create().get();
-      assertTrue(activity != null);
-  }
-
-  @org.junit.Test
-  public void testFindTextView() throws Exception {
-    Activity activity = Robolectric.buildActivity(SimpleActivity.class).create().get();
-    TextView view = Helper.with(activity).find(TextView.class).where("New Text");
-    System.out.println(view.getText());
-    assertNotNull(view);
-  }
+    @org.junit.Test
+    public void testFindTextView() throws Exception {
+        Activity activity = Robolectric.buildActivity(SimpleActivity.class).create().get();
+        TextView view = with(activity).find(TextView.class).where("New Text");
+        // TODO:
+        // 1 - Support returning all results
+        // 2 - Support matchers, i.e. TextView view = with(activity).find(TextView.class).where(textIs("New Text"));
+        System.out.println(view.getText());
+        assertNotNull(view);
+    }
 
     @org.junit.Test
     public void testFindButton() throws Exception {
 
         Activity activity = Robolectric.buildActivity(SimpleActivity.class).create().get();
-        Button view = Helper.with(activity).find(Button.class).where("New Button");
+        Button view = with(activity).find(Button.class).where("New Button");
         System.out.println(view.getText());
         assertNotNull(view);
     }
