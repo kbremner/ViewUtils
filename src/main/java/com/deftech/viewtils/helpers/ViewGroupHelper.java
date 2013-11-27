f93b7c1e125e7c503f3e58161a2f598678aa4af3
kbremner <kbremner@live.co.uk>
2013-11-27 13:01:21 +0000

    Create ViewGroupHelper.java

---
+++ src/main/java/com/deftech/viewtils/helpers/ViewGroupHelper.java
@@ -0,0 +1,15 @@
+package com.deftech.viewtils.helpers;
+
+import android.view.View;
+import android.view.ViewGroup;
+import com.deftech.viewtils.finders.ViewFinder;
+
+public class ViewGroupHelper extends Helper<ViewGroup> {
+    ActivityHelper(ViewGroup instance) {
+        super(instance);
+    }
+
+    public <T extends View> ViewFinder<T> find(Class<T> view){
+        return new ViewFinder<T>(getInstance(), view);
+    }
+}
