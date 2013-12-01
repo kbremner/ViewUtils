38ef1cce3e029eefd97b1866e657fb3e180c156c
Kyle Bremner <kbremner@live.co.uk>
2013-12-01 11:56:19 +0000

    ActivityHelper now internally uses a ViewGroupHelper instance

--- src/main/java/com/deftech/viewtils/ActivityHelper.java
+++ src/main/java/com/deftech/viewtils/ActivityHelper.java
@@ -8,20 +8,18 @@ import com.deftech.viewtils.matchers.Requirement;
 import com.deftech.viewtils.matchers.ViewMatcher;
 
 public class ActivityHelper extends Helper<Activity> {
+    private final ViewGroupHelper groupHelper;
+
     ActivityHelper(Activity instance) {
         super(instance, Activity.class);
+        this.groupHelper = new ViewGroupHelper((ViewGroup)instance.findViewById(android.R.id.content));
     }
 
     public <T extends View> ViewMatcher<T> find(Class<T> view){
-        return new ViewMatcher<T>((ViewGroup)instance.findViewById(android.R.id.content), view);
+        return groupHelper.find(view);
     }
 
     public <T extends View> boolean click(Class<T> viewClass, Requirement<? super T> requirement){
-        T view = find(viewClass).where(requirement);
-        if(view != null){
-            view.performClick();
-            return true;
-        }
-        return false;
+        return groupHelper.click(viewClass, requirement);
     }
 }
