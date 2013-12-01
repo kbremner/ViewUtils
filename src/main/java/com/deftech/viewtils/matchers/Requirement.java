d8870695880a695683b74838c475977bb73588a9
Kyle Bremner <kbremner@live.co.uk>
2013-12-01 01:25:24 +0000

    Added javadoc comments

--- src/main/java/com/deftech/viewtils/matchers/Requirement.java
+++ src/main/java/com/deftech/viewtils/matchers/Requirement.java
@@ -1,6 +1,14 @@
 package com.deftech.viewtils.matchers;
 
-
+/***
+ * Represents a requirement for an object of type {@code T}
+ * @param <T> The type of object that the requirement relates to
+ */
 public interface Requirement<T> {
+    /***
+     * Tests whether {@code instance} matches this requirement
+     * @param instance instance to test
+     * @return true if the instance matches this requirement, else false
+     */
     public boolean matchesRequirement(T instance);
 }
