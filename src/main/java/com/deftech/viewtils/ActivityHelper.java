d67f01452ac01d419c9e1fa6dd464c9d9aa6d8de
Kyle Bremner <kbremner@live.co.uk>
2013-12-06 08:05:02 -0500

    Added javadoc comments

--- src/main/java/com/deftech/viewtils/ActivityHelper.java
+++ src/main/java/com/deftech/viewtils/ActivityHelper.java
@@ -4,9 +4,20 @@ package com.deftech.viewtils;
 import android.app.Activity;
 import android.view.ViewGroup;
 
+/***
+ * Helps interact with an {@link Activity}. When
+ * searching for a view, it starts at the view returned
+ * by {@code activity.findViewById(android.R.id.content)}
+ * @see ViewGroupHelper
+ */
 public class ActivityHelper extends ViewGroupHelper {
     private final Activity activity;
 
+    /***
+     * Create a new instance to help interact with the
+     * provided {@link Activity}
+     * @param instance Activity to be used for carrying out operations
+     */
     ActivityHelper(Activity instance) {
         super((ViewGroup)instance.findViewById(android.R.id.content));
         this.activity = instance;
--- src/main/java/com/deftech/viewtils/ViewGroupHelper.java
+++ src/main/java/com/deftech/viewtils/ViewGroupHelper.java
@@ -5,21 +5,45 @@ import android.view.ViewGroup;
 import com.deftech.viewtils.matchers.Requirement;
 import com.deftech.viewtils.matchers.ViewMatcher;
 
+/***
+ * Helps interact with a {@link ViewGroup}. When
+ * searching for a view, it starts looking in the provided
+ * {@link ViewGroup} and also looks in any nested {@link ViewGroup}'s
+ * @see ActivityHelper
+ */
 public class ViewGroupHelper extends Helper {
 
+    /***
+     * Create a new instance to help interact with the
+     * provided {@link ViewGroup}
+     * @param instance ViewGroup to be used for carrying out operations
+     */
     ViewGroupHelper(ViewGroup instance) {
         super(instance);
     }
 
+    /***
+     * Returns a {@link ViewMatcher} to help find a view of the provided type
+     * within this Helper's ViewGroup
+     * @param view the class that represents the type of view to find
+     * @param <T> the type of view to find
+     * @return a {@link ViewMatcher} that can be used to find a view of the specified type
+     */
     public <T extends View> ViewMatcher<T> find(Class<T> view){
         return new ViewMatcher<T>((ViewGroup) instance, view);
     }
 
+    /***
+     * Attempts to click on a view that meets the provided {@link Requirement}
+     * @param viewClass class that represents the type of view to be clicked
+     * @param requirement a requirement that a view must meet before we click it
+     * @param <T> the type of view to be clicked
+     * @return true if {@code performClick()} for a view that met the requirement returned true, else false
+     */
     public <T extends View> boolean click(Class<T> viewClass, Requirement<? super T> requirement){
         T view = find(viewClass).where(requirement);
         if(view != null){
-            view.performClick();
-            return true;
+            return view.performClick();
         }
         return false;
     }
